cafbab7a5ae6bd00f97aaaf938d0113924cb6f68
ixlammm <islamtazerout3@gmail.com>
2025-04-02 22:33:12 +0100

    Added remote db

--- prisma/migrations/20250402213242_/migration.sql
+++ prisma/migrations/20250402213242_/migration.sql
@@ -1,19 +1,20 @@
+-- CreateEnum
+CREATE TYPE "StaffRole" AS ENUM ('admin', 'reception', 'medecin', 'infirmier', 'radiologue', 'laborantin', 'comptable');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('male', 'female');
+
 -- CreateTable
 CREATE TABLE "Patient" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "age" INTEGER NOT NULL,
-    "gender" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
     "contact" TEXT NOT NULL,
     "email" TEXT,
     "address" TEXT,
-    "city" TEXT,
-    "state" TEXT,
-    "zipCode" TEXT,
-    "insurance" TEXT,
     "status" TEXT NOT NULL,
     "lastVisit" TIMESTAMP(3) NOT NULL,
-    "doctor" TEXT NOT NULL,
     "notes" TEXT,
 
     CONSTRAINT "Patient_pkey" PRIMARY KEY ("id")
@@ -22,18 +23,15 @@ CREATE TABLE "Patient" (
 -- CreateTable
 CREATE TABLE "Staff" (
     "id" TEXT NOT NULL,
+    "userId" TEXT,
     "name" TEXT NOT NULL,
-    "role" TEXT NOT NULL,
-    "department" TEXT NOT NULL,
+    "role" "StaffRole" NOT NULL,
     "status" TEXT NOT NULL,
     "contact" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "joined" TIMESTAMP(3) NOT NULL,
-    "gender" TEXT,
+    "gender" "Gender",
     "address" TEXT,
-    "qualifications" TEXT,
-    "specialization" TEXT,
-    "experience" TEXT,
     "notes" TEXT,
 
     CONSTRAINT "Staff_pkey" PRIMARY KEY ("id")
@@ -42,10 +40,8 @@ CREATE TABLE "Staff" (
 -- CreateTable
 CREATE TABLE "Appointment" (
     "id" TEXT NOT NULL,
-    "patientName" TEXT NOT NULL,
     "patientId" TEXT NOT NULL,
     "doctorId" TEXT NOT NULL,
-    "department" TEXT NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
     "time" TEXT NOT NULL,
     "status" TEXT NOT NULL,
@@ -57,27 +53,12 @@ CREATE TABLE "Appointment" (
 );
 
 -- CreateTable
-CREATE TABLE "Room" (
-    "id" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "floor" TEXT NOT NULL,
-    "beds" INTEGER NOT NULL,
-    "status" TEXT NOT NULL,
-    "patientId" TEXT NOT NULL,
-    "equipment" TEXT,
-    "notes" TEXT,
-
-    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT,
     "pswdHash" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "emailVerified" TIMESTAMP(3),
-    "image" TEXT,
+    "role" "StaffRole" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -121,31 +102,14 @@ CREATE TABLE "VerificationToken" (
     CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("identifier","token")
 );
 
--- CreateTable
-CREATE TABLE "Authenticator" (
-    "credentialID" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "providerAccountId" TEXT NOT NULL,
-    "credentialPublicKey" TEXT NOT NULL,
-    "counter" INTEGER NOT NULL,
-    "credentialDeviceType" TEXT NOT NULL,
-    "credentialBackedUp" BOOLEAN NOT NULL,
-    "transports" TEXT,
-
-    CONSTRAINT "Authenticator_pkey" PRIMARY KEY ("userId","credentialID")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "Patient_email_key" ON "Patient"("email");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Staff_userId_key" ON "Staff"("userId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Staff_email_key" ON "Staff"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Appointment_patientId_key" ON "Appointment"("patientId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Room_patientId_key" ON "Room"("patientId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
@@ -153,23 +117,17 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 -- CreateIndex
 CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
 
--- CreateIndex
-CREATE UNIQUE INDEX "Authenticator_credentialID_key" ON "Authenticator"("credentialID");
+-- AddForeignKey
+ALTER TABLE "Staff" ADD CONSTRAINT "Staff_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "Staff"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Room" ADD CONSTRAINT "Room_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "Staff"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Authenticator" ADD CONSTRAINT "Authenticator_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
